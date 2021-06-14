f384c50b8f52c362c6d094d62d03c3ee73c62ab2
GustavoB0rges <gustavoborges832@gmail.com>
2021-06-13 22:28:14 -0300

    ajustando owl carousel

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$('.owl-carousel').owlCarousel({
+$('#owl-carousel1').owlCarousel({
     loop: false,
     margin: 10,
     nav: true,
@@ -20,4 +20,50 @@ $('.owl-carousel').owlCarousel({
             items: 9,
         }
     }
-})
+});
+$('#owl-carousel2').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: true,
+    responsiveClass: true,
+    navigation: true,
+    navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    dots: false,
+    responsive: {
+        0: {
+            items: 2,
+        },
+        600: {
+            items: 3,
+        },
+        900: {
+            items: 6,
+        },
+        1300: {
+            items: 4,
+        }
+    }
+});
+$('#owl-carousel3').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: true,
+    responsiveClass: true,
+    navigation: true,
+    navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    dots: false,
+    responsive: {
+        0: {
+            items: 2,
+        },
+        600: {
+            items: 3,
+        },
+        900: {
+            items: 6,
+        },
+        1300: {
+            items: 9,
+        }
+    }
+});
