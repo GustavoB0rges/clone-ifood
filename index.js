57d92357f442196e4663fc4bd4ab1c3b128eecd6
GustavoB0rges <gustavoborges832@gmail.com>
2021-06-16 14:55:33 -0300

    ajustando header

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function(popoverTrigger) {
+    return new bootstrap.Popover(popoverTrigger);
+});
+
+
 $('#owl-carousel1').owlCarousel({
     loop: false,
     margin: 10,
